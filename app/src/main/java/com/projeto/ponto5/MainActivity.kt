675da16a2987e9b003f56d90c156bdd2ee88e945
Luiz Gustavo Dias Pulz <luizgustavodiaspulz@gmail.com>
2025-10-06 17:59:22 -0300

    Imagens icone

--- app/src/main/java/com/projeto/ponto5/MainActivity.kt
+++ app/src/main/java/com/projeto/ponto5/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.lifecycleScope
@@ -103,9 +106,11 @@ class MainActivity : ComponentActivity() {
 
                 Column(
                     modifier = Modifier
+                        .background(MaterialTheme.colorScheme.background)
                         .fillMaxSize()
                         .padding(16.dp),
-                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     OutlinedTextField(
                         value = cpf,
